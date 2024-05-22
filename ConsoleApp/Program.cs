9f532fff6087aff293db0a06c731d90209f828ca
Anton Gridushko <gridushko.anton@yahoo.com>
2024-05-23 00:36:23 +0300

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+	static List<string> UniquePermutations(string inputStr)
+	{
+		var perms = new HashSet<string>(GetPermutations(inputStr));
+		return perms.ToList();
+	}
+
+	static IEnumerable<string> GetPermutations(string inputStr)
+	{
+		if (inputStr.Length == 1)
+		{
+			yield return inputStr;
+		}
+		else
+		{
+			foreach (var perm in GetPermutations(inputStr.Substring(1)))
+			{
+				for (int i = 0; i <= perm.Length; i++)
+				{
+					yield return perm.Insert(i, inputStr[0].ToString());
+				}
+			}
+		}
+	}
+
+	static void Main()
+	{
+		string inputStr = "aabb";
+		var result = UniquePermutations(inputStr);
+		foreach (var perm in result)
+		{
+			Console.WriteLine(perm);
+		}
+
+		Console.ReadLine();
+	}
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+	static List<T> UniqueInOrder<T>(IEnumerable<T> sequence)
+	{
+		List<T> uniqueList = new List<T>();
+		T prev = default(T);
+
+		foreach (T item in sequence)
+		{
+			if (EqualityComparer<T>.Default.Equals(item, prev) == false)
+			{
+				uniqueList.Add(item);
+			}
+			prev = item;
+		}
+
+		return uniqueList;
+	}
+
+	static void Main()
+	{
+		string sequenceStr = "AAAABBBCCDAABBB";
+		var result = UniqueInOrder(sequenceStr);
+		foreach (var item in result)
+		{
+			Console.Write(item + " ");
+		}
+	}
+}
